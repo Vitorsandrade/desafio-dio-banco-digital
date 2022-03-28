0e2d8c536ec5b2dfc92739f5540b86788754b965
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-03-28 19:35:02 -0300

    First commit

---
+++ desafio-dio-banco-digital/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Cliente vitor = new Cliente();
+		vitor.setNome("vitor");
+		
+	
+		Conta cc = new ContaCorrente(vitor);		
+		Conta poupanca = new ContaPoupanca(vitor);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+}
